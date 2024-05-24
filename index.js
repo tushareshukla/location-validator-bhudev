e24ec0e9c4eda2720a984839ce216c85e75c0065
tusharetraya <tusharshukla@traya.health>
2024-05-24 16:16:03 +0530

    1.0.5

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 class LocationValidator {
-    getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
-      const R = 6371e3; // Radius of the Earth in meters
-      const dLat = (lat2 - lat1) * Math.PI / 180;
-      const dLon = (lon2 - lon1) * Math.PI / 180;
-      const a = 
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      const distance = R * c; // Distance in meters
-      return distance;
-    }
-  
-    isWithinDistance(lat1, lon1, lat2, lon2, maxDistanceMeters) {
-      const distance = this.getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2);
-      return distance <= maxDistanceMeters;
-    }
+  deg2rad(deg) {
+    return deg * (Math.PI / 180);
   }
-  
-  module.exports = LocationValidator;
-  
+
+  getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
+    const earthRadius = 6371e3; // Radius of the Earth in meters
+    const dLat = this.deg2rad(lat2 - lat1);
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) *
+        Math.cos(this.deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadius * c; // Distance in meters
+    return distance;
+  }
+
+  isWithinDistance(lat1, lon1, lat2, lon2, maxDistanceMeters) {
+    const distance = this.getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2);
+    return distance <= maxDistanceMeters;
+  }
+}
+
+module.exports = LocationValidator;
